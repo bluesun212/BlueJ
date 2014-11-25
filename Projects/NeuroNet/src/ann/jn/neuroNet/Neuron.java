53b27ca303e1c84d190e9b83fb723c9d65ccd26b
Jared Jonas <bluesun212@gmail.com>
2014-11-25 15:01:02 -0800

    Fixed some equation stuff.

--- Projects/NeuroNet/src/ann/jn/neuroNet/Neuron.java
+++ Projects/NeuroNet/src/ann/jn/neuroNet/Neuron.java
@@ -30,13 +30,6 @@ public class Neuron implements Serializable {
 		 * @return the result of evaluating the activation function for the given weighted input
 		 */
 		public float evaluate(float x);
-		
-		/**
-		 * Evaluates the derivative of the INeuronActivationFunction for the given summation of input values.
-		 * @param x the summation of the inputs multiplied by their weights
-		 * @return the result of evaluating the derivative of activation function for the given weighted input
-		 */
-		public float evaluateDerivative(float x);
 	}
 
 	/**
@@ -177,12 +170,6 @@ public class Neuron implements Serializable {
 	public static final INeuronActivationFunction DEFAULT_FUNCTION = new INeuronActivationFunction() {
 		@Override
 		public float evaluate(float x) {
-			return (float) (1 / (1 + Math.pow(Math.E, -x)));
-		}
-
-		@Override
-		public float evaluateDerivative(float x) {
-			return evaluate(x) * (1 - evaluate(x));
+			return (float) (1 / (1 + Math.exp(-x)));
 		}};
-
 }
