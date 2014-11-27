50cc316ca82aa6e8c80974aea76cbb633c2cb703
Jared Jonas <bluesun212@gmail.com>
2014-11-26 23:35:33 -0800

    Added the creature and sprites.

--- Projects/WallAvoid/src/ann/jn/genetic/Main.java
+++ Projects/WallAvoid/src/ann/jn/genetic/Main.java
@@ -1,9 +1,20 @@
 package ann.jn.genetic;
 
 import com.bluesun212.components.Window;
+import com.bluesun212.graphics.ImageTexture;
+import com.bluesun212.resources.ResourceManager;
 
 public class Main {
 	public static void main(String[] args) {
 		Window.create(250, 250);
+		
+		ResourceManager.startResourceBlock("data");
+		new ImageTexture("res/images/player.png").setName("ai");
+		new ImageTexture("res/images/wall.png").setName("wall");
+		ResourceManager.endResourceBlock("data");
+		ResourceManager.loadBlock("data");
+		ResourceManager.blockUntilBlockLoads("data");
+		
+		Window.switchScenes(new TrainScene());
 	}
 }
