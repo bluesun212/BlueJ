a0547f13346ca1464fde08a3fd1eb649aa7e7ced
sandyhill <utznicholas@gmail.com>
2014-12-03 16:15:30 -0800

    Added Neuron Bias workaround to WeightMap stuff.

--- Projects/NeuroNet/src/ann/jn/teach/gen/WeightMap.java
+++ Projects/NeuroNet/src/ann/jn/teach/gen/WeightMap.java
@@ -25,7 +25,7 @@ public class WeightMap implements Serializable {
 			weights[x - 1] = new float[neurons[x]][];
 			
 			for (int y = 0; y < neurons[x]; y++) {
-				weights[x][y] = new float[neurons[x - 1]];
+				weights[x][y] = new float[neurons[x - 1]++];//extra float to store bias
 				for (int z = 0; z < weights[x][y].length; z++) {
 					if (randomize) {
 						weights[x][y][z] = (float) ( 2 * Math.random()) - 1;
@@ -39,23 +39,56 @@ public class WeightMap implements Serializable {
 	}
 	
 	/**
+	 * <p>
 	 * Returns the weights stored for the {@link Neuron} with the given index in the given <code>layer</code>.
+	 * </p>
+	 * <p>
+	 * <b><em>Note:</em></b> The first value returned by this function is the <em>bias</em> of the neuron, not 
+	 * the first weight.
+	 * </p>
 	 * @param layer the layer of the target Neuron
 	 * @param neuron the index of the target Neuron in the given layer
-	 * @return the weights stored for the target Neuron
+	 * @return the weights and bias stored for the target Neuron
 	 */
 	public synchronized float[] getWeightsForNeuron(int layer, int neuron) {
 		return weights[layer][neuron];
 	}
 	
 	/**
+	 * Returns the bias value stored for the {@link Neuron} <code>neuron</code> in the given <code>layer</code>.
+	 * @param layer the layer of the target Neuron
+	 * @param neuron the Neuron in the layer
+	 * @return the bias value stored for the target Neuron
+	 */
+	public synchronized float getBiasForNeuron(int layer, int neuron) {
+		return weights[layer][neuron][0];
+	}
+	
+	/**
+	 * <p>
 	 * Sets the weights for the Neuron with the given index <code>neuron</code> in the layer <code>layer</code>.
+	 * </p>
+	 * <p>
+	 * <b><em>Note:</em></b> The first element of <code>weights</code> should be the <em>bias</em> of the Neuron,
+	 * not the first weight.
+	 * </p>
 	 * @param layer the layer of the target Neuron
 	 * @param neuron the index of the target Neuron
 	 * @param weights the weights to assign the target Neuron
 	 */
 	public synchronized void setWeightsForNeuron(int layer, int neuron, float[] weights) {
 		this.weights[layer][neuron] = weights;
+	}
+	
+	/**
+	 * Sets the bias value stored for the {@link Neuron} <code>neuron</code> in the layer <code>layer</code>
+	 * to <code>bias</code>.
+	 * @param layer the layer of the target Neuron
+	 * @param neuron the Neuron in the target layer
+	 * @param bias the bias value for the target Neuron
+	 */
+	public synchronized void setBiasForNeuron(int layer, int neuron, float bias) {
+		this.weights[layer][neuron][0] = bias;
 	}
 	
 	/**
@@ -76,8 +109,13 @@ public class WeightMap implements Serializable {
 	}
 	
 	/**
-	 * Returns the number of weights that are stored for the Neuron of index <code>neuron</code>
+	 * <p>
+	 * Returns the number of weights that are stored for the {@link Neuron} of index <code>neuron</code>
 	 * in the layer <code>layer</code>.
+	 * </p>
+	 * <p>
+	 * <b><em>Note:</em></b> The bias value of the Neuron <em>is</em> included in the weight count.
+	 * </p>
 	 * @param layer the layer of the Neuron to check
 	 * @param neuron the index of the Neuron to check
 	 * @return the number of weights stored for the target Neuron
@@ -96,7 +134,7 @@ public class WeightMap implements Serializable {
 	}
 	
 	/**
-	 * Sets all of the weights in the layer <code>layer</code>.
+	 * Sets all of the weights and the bias in the layer <code>layer</code>.
 	 * @param layer the index of the layer to set
 	 * @param data the data to set the layer <code>layer</code> to
 	 */
